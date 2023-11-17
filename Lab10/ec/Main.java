960ad8caa74d13059c93977a4c36370bf46ccc69
johngantner <127889384+johngantner@users.noreply.github.com>
2023-11-16 22:47:20 -0500

    Create Main.java

---
+++ Lab10/ec/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main!");
+        Utils.printMessage(); // Calling the method from Utils.java
+    }
+}
